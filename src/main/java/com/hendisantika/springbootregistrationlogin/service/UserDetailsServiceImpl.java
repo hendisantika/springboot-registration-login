56547a71e3122de70bd29a3cedc8c7906b43cc36
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-28 06:31:09 +0700

    #13 - Add UserDetailsServiceImpl

---
+++ src/main/java/com/hendisantika/springbootregistrationlogin/service/UserDetailsServiceImpl.java
@@ -0,0 +1,46 @@
+package com.hendisantika.springbootregistrationlogin.service;
+
+import com.hendisantika.springbootregistrationlogin.entity.Role;
+import com.hendisantika.springbootregistrationlogin.entity.User;
+import com.hendisantika.springbootregistrationlogin.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-registration-login
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 28/11/20
+ * Time: 06.30
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findByUsername(username);
+        if (user == null) throw new UsernameNotFoundException(username);
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        for (Role role : user.getRoles()) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                grantedAuthorities);
+    }
+}
