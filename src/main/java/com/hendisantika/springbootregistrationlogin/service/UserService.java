e3d40e130b70962b18ffb338a20d5567ac86c095
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-28 06:32:10 +0700

    #14 - Add UserService

---
+++ src/main/java/com/hendisantika/springbootregistrationlogin/service/UserService.java
@@ -0,0 +1,41 @@
+package com.hendisantika.springbootregistrationlogin.service;
+
+import com.hendisantika.springbootregistrationlogin.entity.User;
+import com.hendisantika.springbootregistrationlogin.repository.RoleRepository;
+import com.hendisantika.springbootregistrationlogin.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-registration-login
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 28/11/20
+ * Time: 06.31
+ */
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public void save(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles(new HashSet<>(roleRepository.findAll()));
+        userRepository.save(user);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
