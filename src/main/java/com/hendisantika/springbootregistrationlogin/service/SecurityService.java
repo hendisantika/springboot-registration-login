35aaab74de178f37f2967de7935594964fd51d85
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-28 06:29:37 +0700

    #12 - Add autoLogin in SecurityService

--- src/main/java/com/hendisantika/springbootregistrationlogin/service/SecurityService.java
+++ src/main/java/com/hendisantika/springbootregistrationlogin/service/SecurityService.java
@@ -3,6 +3,7 @@ package com.hendisantika.springbootregistrationlogin.service;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -33,5 +34,18 @@ public class SecurityService {
         }
 
         return null;
+    }
+
+    public void autoLogin(String username, String password) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+
+        authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+
+        if (usernamePasswordAuthenticationToken.isAuthenticated()) {
+            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            log.debug(String.format("Auto login %s successfully!", username));
+        }
     }
 }
