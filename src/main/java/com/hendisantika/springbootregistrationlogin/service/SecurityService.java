fe10f1129dd8be5c033cca1643fe1c335f77f0dd
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-28 06:29:15 +0700

    #11 - Add findLoggedInUsername in SecurityService

--- src/main/java/com/hendisantika/springbootregistrationlogin/service/SecurityService.java
+++ src/main/java/com/hendisantika/springbootregistrationlogin/service/SecurityService.java
@@ -3,6 +3,8 @@ package com.hendisantika.springbootregistrationlogin.service;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
@@ -23,4 +25,13 @@ public class SecurityService {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    public String findLoggedInUsername() {
+        Object userDetails = SecurityContextHolder.getContext().getAuthentication().getDetails();
+        if (userDetails instanceof UserDetails) {
+            return ((UserDetails) userDetails).getUsername();
+        }
+
+        return null;
+    }
 }
