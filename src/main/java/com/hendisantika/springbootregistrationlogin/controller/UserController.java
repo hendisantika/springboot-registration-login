2289036bf79c333b881f01639df7d63c2aad98be
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-29 05:36:39 +0700

    #21 - Add registration API in UserController

--- src/main/java/com/hendisantika/springbootregistrationlogin/controller/UserController.java
+++ src/main/java/com/hendisantika/springbootregistrationlogin/controller/UserController.java
@@ -7,7 +7,10 @@ import com.hendisantika.springbootregistrationlogin.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * Created by IntelliJ IDEA.
@@ -33,5 +36,18 @@ public class UserController {
     public String registration(Model model) {
         model.addAttribute("userForm", new User());
         return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult) {
+        userValidator.validate(userForm, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+
+        userService.save(userForm);
+        securityService.autoLogin(userForm.getUsername(), userForm.getPasswordConfirm());
+        return "redirect:/welcome";
     }
 }
