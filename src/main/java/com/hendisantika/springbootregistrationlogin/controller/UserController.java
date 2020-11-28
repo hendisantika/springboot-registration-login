e9a8ae6b237bfef024ea169ef070dffcbd5ada85
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-29 05:37:06 +0700

    #22 - Add login API in UserController

--- src/main/java/com/hendisantika/springbootregistrationlogin/controller/UserController.java
+++ src/main/java/com/hendisantika/springbootregistrationlogin/controller/UserController.java
@@ -50,4 +50,20 @@ public class UserController {
         securityService.autoLogin(userForm.getUsername(), userForm.getPasswordConfirm());
         return "redirect:/welcome";
     }
+
+    @GetMapping("/login")
+    public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("error", "Your username and password is invalid.");
+
+        if (logout != null)
+            model.addAttribute("message", "You have been logged out successfully.");
+
+        return "login";
+    }
+
+    @GetMapping({"/", "/welcome"})
+    public String welcome(Model model) {
+        return "welcome";
+    }
 }
