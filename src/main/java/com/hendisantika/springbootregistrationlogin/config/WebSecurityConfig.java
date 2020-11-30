add87bcb9b95b15f8ca97e777286ebd611b03cec
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-30 07:02:40 +0700

    #34 - Bug Fix assets path

--- src/main/java/com/hendisantika/springbootregistrationlogin/config/WebSecurityConfig.java
+++ src/main/java/com/hendisantika/springbootregistrationlogin/config/WebSecurityConfig.java
@@ -37,7 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/resources/**", "/registration").permitAll()
+                .antMatchers("/resources/**", "/registration", "/webjars/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
